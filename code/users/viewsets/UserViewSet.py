f455ef6269d757407445087406ebb5a2bd6c333a
yamkant <dev.yamkim@gmail.com>
2023-02-04 23:54:08 +0900

    feat: #1 [App: users] 유저 관련 앱 생성 및 조회를 위한 ViewSet 추가

--- code/users/viewsets/UserViewSet.py
+++ code/users/viewsets/UserViewSet.py
@@ -3,16 +3,17 @@ from users.models import DjangoUser, User
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework import status
 
 class UserViewSet(viewsets.ModelViewSet): 
-    """
-    This viewset automatically provides `list`, `create`, `retrieve`,
-    `update` and `destroy` actions.
-
-    Additionally we also provide an extra `highlight` action.
-    """
-    queryset = User.objects.all()
+    # queryset = User.objects.all()
     # serializer_class = UserSerializer
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    def list(self, req):
+        queryset = User.objects.filter().values('nickname')
+        return Response(queryset, status=status.HTTP_200_OK)
 
+user_list = UserViewSet.as_view({
+    'get': 'list',
+})
